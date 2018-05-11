f3c99cf4c3357fb1ec0e7bc760ac8e74f5d15a04
Sinuee Hernández <sinuee@gmail.com>
2018-05-11 07:40:58 -0500

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,35 @@
+// Framework Modules
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
-import { AuthGuardService } from './auth/auth-guard.service';
-import { AuthService } from './auth/auth.service';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeModule } from './recipes/recipe.module';
-import { RecipeService } from './services/recipe.service';
-import { ShoppingListService } from './services/shopping-list.service';
-import { DataStorageServices } from './shared/data-storage.services';
+// Feature Modules
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app.routing.module';
+import { RecipeModule } from './recipes/recipe.module';
+// Services
+import { AuthGuardService } from './auth/auth-guard.service';
+import { AuthService } from './auth/auth.service';
+import { DataStorageServices } from './shared/data-storage.services';
+import { RecipeService } from './services/recipe.service';
+import { ShoppingListService } from './services/shopping-list.service';
+// Components
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    AuthModule,
     RecipeModule,
     ShoppingListModule,
     SharedModule
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingComponent } from './shopping/shopping.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuardService } from './auth/auth-guard.service';
+import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'signup', component: SignupComponent},
-    {path: 'signin', component: SigninComponent}
 ];
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
